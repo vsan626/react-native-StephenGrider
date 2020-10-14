d837cf37a3ea60a26ae211cdd044a876d9381c0a
Danny (vai) San <vai.quoc.san@gmail.com>
2020-10-13 17:24:01 -0700

    utilizing flatlist component for arrays, similar usage to map function in regular react

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { View, Button, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Text, FlatList } from 'react-native';
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+  console.log(colors);
   return (
     <View>
-      <Button title="Add a Color" />
-      <View
-        style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
+      <Button
+        title="Add a Color"
+        onPress={() => {
+          setColors([...colors, randomRgb()]);
+        }}
+      />
+
+      <FlatList
+      keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{ height: 100, width: 100, backgroundColor: item}}
+            />
+          );
+        }}
       />
     </View>
   );
@@ -17,7 +33,7 @@ const randomRgb = () => {
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
 
-  return `rgb(${red}, ${green}, ${blue})`
+  return `rgb(${red}, ${green}, ${blue})`;
 };
 
 export default ColorScreen;
