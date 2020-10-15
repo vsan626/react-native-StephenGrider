c519782de00b66a7e2a2fc253911f5af56e6a54f
Danny (vai) San <vai.quoc.san@gmail.com>
2020-10-14 17:27:49 -0700

    utilized reducer for color changing app and reformatted count app with useReducer

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,24 +1,37 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+
+const reducer = (state, action) => {
+  //TODO: state of count 
+  //TODO: action === { type: 'increment' || 'decrement', payload: }
+  switch (action.type) {
+    case 'increment_count':
+      return {...state, count: state.count + action.payload}
+    case 'decrement_count':
+      return {...state, count: state.count + action.payload}
+    default:
+      return state
+  }
+}
 
 const CounterScreen = () => {
   //TODO: fix this
-  let [counter, setCounter] = useState(0)
+  let [state, dispatch] = useReducer(reducer, {count: 0})
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          setCounter(counter+1);
+          dispatch({type: 'increment_count', payload: 1})
         }}
       />
       <Button
         title="Increase"
         onPress={() => {
-          setCounter(counter-1);
+          dispatch({type: 'increment_count', payload: -1})
         }}
       />
-      <Text>Current Counter: {counter}</Text>
+      <Text>Current Counter: {state.count}</Text>
     </View>
   );
 };
