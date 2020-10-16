75506a914e28146c8c6c9ed9aec021593d465fe4
Danny (vai) San <vai.quoc.san@gmail.com>
2020-10-15 19:34:34 -0700

    yelp api connected

---
+++ food/src/api/apiKeyExample.js
@@ -0,0 +1,3 @@
+const EXAMPLE_yelpApiKey =
+  'YOUR_API_KEY';
+export default EXAMPLE_yelpApiKey;
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term,
+        location: 'Los Angeles'
+      }
+    });
+    setResults(response.data.businesses);
+  }
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={searchApi}
+      />
       <Text>Search Screen</Text>
-      <Text>Test data: {term}</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
