116f9258cd5fe84afeba8533c54aea1331e38c3f
Danny (vai) San <vai.quoc.san@gmail.com>
2020-10-15 17:14:15 -0700

    updated submodule

---
+++ food/src/screens/SearchScreen.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+
+const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  return (
+    <View>
+      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} />
+      <Text>Search Screen</Text>
+      <Text>Test data: {term}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
