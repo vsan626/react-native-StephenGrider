47b120f02ec53fa99cbe4c95721c98aa5965bb98
Danny (vai) San <vai.quoc.san@gmail.com>
2020-10-19 14:56:52 -0700

    restaurant app completed

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import ResultsDetail from './ResultsDetail';
+import {withNavigation } from 'react-navigation'; //! skipps having to send props down from parent component
 
-const ResultsList = ({title}) => {
+const ResultsList = ({ title, results, navigation }) => {
+  if(!results.length) {
+    return null;
+  }
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={results}
+        keyExtractor={(results) => results.id}
+        renderItem={({ item }) => {
+          return (
+            //! TouchableOpacity allows for certain location of screen to be pressed. Navigation.navigate brings us to component specified on App.js. Second parameter is information we can pass to the component
+            <TouchableOpacity onPress={() => navigation.navigate('ResultsShow', {id: item.id})}>
+              <ResultsDetail result={item}/>
+            </TouchableOpacity>
+          );
+        }}
+      />
     </View>
   );
 };
@@ -12,7 +31,12 @@ const ResultsList = ({title}) => {
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5
+  },
+  container: {
+    marginBottom: 10
   }
 });
-export default ResultsList;
+export default withNavigation(ResultsList);
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 import useResults from '../hooks/useResults';
@@ -9,19 +9,33 @@ const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
+  const filterResultsByPrice = (price) => {
+    return results.filter((results) => {
+      return results.price === price;
+    });
+  };
+
   return (
-    <View>
+    //add flex: 1 in View to help display content in available size of screen OR can use <> </>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
-      <ResultsList title='Cost Effective'/>
-      <ResultsList title='Bit Pricier'/>
-      <ResultsList title='Big Spender'/>
-    </View>
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice('$')}
+          title="Cost Effective"
+        />
+        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+        <ResultsList
+          results={filterResultsByPrice('$$$')}
+          title="Big Spender"
+        />
+      </ScrollView>
+    </>
   );
 };
 
